58de5a90c43f1e3f8298b4d2219e04b2602ca0ee
Tolkachev Kirill <lavcraft@gmail.com>
2017-05-26 01:17:01 +0300

    #1 Add dev profile in EPP

--- src/main/java/conference/spring/boot/starter/profiles/ProfilesEnvironmentPostProcessor.java
+++ src/main/java/conference/spring/boot/starter/profiles/ProfilesEnvironmentPostProcessor.java
@@ -1,6 +1,5 @@
 package conference.spring.boot.starter.profiles;
 
-import com.sun.tools.javac.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.core.env.ConfigurableEnvironment;
@@ -12,9 +11,11 @@ import org.springframework.core.env.ConfigurableEnvironment;
 public class ProfilesEnvironmentPostProcessor implements EnvironmentPostProcessor {
   @Override
   public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
-    List<String> profiles = List.from(environment.getActiveProfiles());
-    if (profiles.contains("production") && profiles.contains("debug")) {
-      environment.addActiveProfile("trace");
+    String os = System.getProperty("os.name");
+
+    if (os.equalsIgnoreCase("Mac Os X")) {
+      environment.addActiveProfile("dev");
     }
+
   }
 }
